aba82460d10c1a3818736eb0be3d4b36299825ba
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2026-01-05 17:19:01 +0700

    Customize UI for customer

--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -112,9 +112,9 @@ export class UpdateCustomerDto {
   @IsString()
   invoiceCccdCmnd?: string;
 
-  @IsOptional()
-  @IsString()
-  invoiceBankAccount?: string;
+  // @IsOptional()
+  // @IsString()
+  // invoiceBankAccount?: string;
 
   @IsOptional()
   @IsEmail()
