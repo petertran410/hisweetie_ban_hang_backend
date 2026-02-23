7ff19d2c0b475e7c2ed56a4bea267059e00917c6
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2026-02-23 18:53:23 +0700

    Fix update deliveries invoice

--- src/packing-slips/dto/update-packing-slip.dto.ts
+++ src/packing-slips/dto/update-packing-slip.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePackingSlipDto } from './create-packing-slip.dto';
+import { Type } from 'class-transformer';
 
-export class UpdatePackingSlipDto extends PartialType(CreatePackingSlipDto) {}
+export class UpdatePackingSlipDto extends PartialType(CreatePackingSlipDto) {
+  @Type(() => Number)
+  cashAmount?: number;
+
+  @Type(() => Number)
+  feeGuiBen?: number;
+
+  @Type(() => Number)
+  feeGrab?: number;
+
+  @Type(() => Number)
+  cuocGuiHang?: number;
+}
