ebf74ebc3c65fd00dcdd67c529308f22be8e37ca
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2026-06-10 14:42:18 +0700

    Fix search customer

--- src/packing-loadings/packing-loadings.module.ts
+++ src/packing-loadings/packing-loadings.module.ts
@@ -3,9 +3,10 @@ import { PackingLoadingsController } from './packing-loadings.controller';
 import { PackingLoadingsService } from './packing-loadings.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuditLogsModule } from '../audit-logs/audit-logs.module';
+import { LarkSyncModule } from '../lark-sync/lark-sync.module';
 
 @Module({
-  imports: [PrismaModule, AuditLogsModule],
+  imports: [PrismaModule, AuditLogsModule, LarkSyncModule],
   controllers: [PackingLoadingsController],
   providers: [PackingLoadingsService],
   exports: [PackingLoadingsService],
